0220f9c6a85ac103741c4cf40d143d9c4b980e8a
alexvieru1 <vieru.alexandruu@gmail.com>
2024-10-31 22:01:23 +0200

    mobile keyboard test

--- app/components/ui/placeholders-and-vanish-input.tsx
+++ app/components/ui/placeholders-and-vanish-input.tsx
@@ -200,6 +200,7 @@ export const PlaceholdersAndVanishInput = forwardRef<HTMLInputElement, {
         ref={inputRef}
         value={value}
         type="text"
+        enterKeyHint="send"
         className={cn(
           "w-full relative text-[18px] z-50 border-none dark:text-white bg-transparent text-black h-full rounded-full focus:outline-none focus:ring-0 pl-4 sm:pl-10 pr-20",
           animating && "text-transparent dark:text-transparent"
